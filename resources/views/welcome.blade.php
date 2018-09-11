c0490e1e202fcc159e6da653c4882a1504e70d88
omarbenj <43155372+omarbenj@users.noreply.github.com>
2018-09-11 17:28:30 -0500

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Esjsjss
+                    Esjsjss   ddddddd
                 </div>
 
                 
